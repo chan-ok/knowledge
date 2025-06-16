fa2aeb8b83f4f7b48a3a9bda6b37bfc11e002c43
Kim Chanho <kiss.yagni.dry@gmail.com>
2025-06-17 00:56:51 +0900

    fix(vite.config): TanStackRouterVite import 수정 및 플러그인 순서 변경
    TanStackRouterVite is Deprecated

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -7,14 +7,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({
+    react(),
+    tailwindcss(),
+    tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
       routesDirectory: './src/app/routes',
       generatedRouteTree: './src/shared/utils/router/routeTree.gen.ts',
     }),
-    react(),
-    tailwindcss(),
     tsconfigPaths(),
   ],
 });
